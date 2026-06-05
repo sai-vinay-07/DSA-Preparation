ad4ec6878b160a1b8b7b6e18fd7e669b84f4a2bf
SAI VINAY GUTTULA <saivinayguttula007@gmail.com>
2026-06-05 10:58:26 +0530

    Time: 17 ms (78.6%), Space: 49.8 MB (32.5%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,28 +1,9 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-
-        int[] freq = new int[20001];
-
-        for(int num : nums){
-            freq[num + 10000]++;
-        }
-
-        int sum = 0;
-        boolean take = true;
-
-        for(int i = 0; i < 20001; i++){
-
-            while(freq[i] > 0){
-
-                if(take){
-                    sum += i - 10000;
-                }
-
-                take = !take;
-                freq[i]--;
-            }
-        }
-
-        return sum;
+     Arrays.sort(nums);
+     int m=0;
+        for(int i=0;i<nums.length;i+=2){
+            m+=nums[i];
+        }return m;
     }
 }
