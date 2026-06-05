5c0099469f79ade3d5be53a9b1dd438249061fa6
SAI VINAY GUTTULA <saivinayguttula007@gmail.com>
2026-06-05 10:34:31 +0530

    Time: 0 ms (100%), Space: 61.2 MB (68.24%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,7 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        
-        int n = nums.length;
-        
-        k = k % n;
-        
-        reverseArr(0, n-1, nums);
-        
-        reverseArr(0, k-1, nums);
 
-        reverseArr(k, n-1, nums);
 
-        System.out.print(nums);
-
-    }
-
-    public int[] reverseArr(int start, int end, int[] arr){
+    public void reverseArr(int start, int end, int[] arr){
 
         while(start < end){
             int temp = arr[start];
@@ -24,7 +10,23 @@ class Solution {
             start++;
             end--;
         }
-
-        return arr;
     }
+
+    public void rotate(int[] nums, int k) {
+        
+        int n = nums.length;
+        
+        if( k > n){
+            k = k % n;
+        }
+        
+        reverseArr(0, n-1, nums);
+        
+        reverseArr(0, k-1, nums);
+
+        reverseArr(k, n-1, nums);
+
+    }
+
+ 
 }
